b81a68f96df31c72287a0d327ba300c851eaa3e4
Levi Frank <LeviOrionFrank@gmail.com>
2022-06-02 18:09:15 -0400

    made some checkboxes, they don't word yet though. :/

--- index.js
+++ index.js
@@ -9,13 +9,21 @@ function addItem(e) {
         text,
         done: false
     };
+
     items.push(item);
+    populateList(items, itemsList);
     this.reset();
 }
 
 function populateList(plates = [], platesList) {
-
-
+    platesList.innerHTML = plates.map((plate, i) => {
+        return `
+            <li>
+                <input type="checkbox" data-index=${i} id="item${i}" ${plate.done ? 'checked' : ''} />
+                <label for="items${i}">${plate.text} </label>
+            </li>
+        `; 
+    }).join('');
 }
 
 addItems.addEventListener('submit', addItem);
